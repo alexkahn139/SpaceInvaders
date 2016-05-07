1cc5de930951d6e070a6421c70b1dbcd8b40b0ec
alexkahn139 <alexandrekahn139@gmail.com>
2016-05-07 21:33:37 +0200

    Nu wordt het menu automatisch hertekend bij het naar boven of naar beneden gaan

--- rotator.rkt
+++ rotator.rkt
@@ -21,21 +21,24 @@
   (define staat 0)
   (define positie-x 0.45) ;posities zo gekozen dat het mooi uitkwam
   (define positie-y 0.4)
-  
-  (define (up!) ;reageert als je met het pijltje naar boven gaat
+
+  (define (up! teken-adt) ;reageert als je met het pijltje naar boven gaat
     (if (> staat 0)
         (begin
           (set! staat (- staat 1))
           (set! positie-y (- positie-y 0.18))) ;zorgt dat het mooi uitkomt, is omdat het menu 1 foto is
-        'ok))
-  
-  (define (down!) ;reageert als je met het pijltje naar boven gaat
+        'ok)
+    (teken! teken-adt))
+
+  (define (down! teken-adt)
+    ;reageert als je met het pijltje naar boven gaat
     (if (< staat 2)
         (begin
           (set! staat (+ staat 1))
           (set! positie-y (+ positie-y 0.18))) ;zorgt dat het mooi uitkomt, is omdat het menu 1 foto is
-        'ok))
-  
+        'ok)
+    (teken! teken-adt))
+
   ;Geeft mee aan het teken-adt om alles te tekenen
   (define (teken! teken-adt)
     ((teken-adt 'teken-rotator!) dispatch-rotator))
